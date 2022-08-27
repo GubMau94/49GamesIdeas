72386036ee5c5ca0c4c4dce3fbb7848cf93d3d80
GubMau94 <guberman.mn@gmail.com>
2022-08-27 12:43:14 +0200

    Finished
    Other options can be added, like:
     - time of game
     - number of moves done
     - best record (less moves done)

--- SlidingPuzzle/SlidingPuzzle/Assets/_Script/GameManager.cs
+++ SlidingPuzzle/SlidingPuzzle/Assets/_Script/GameManager.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private Transform buttonParent, positionParent;
+    [SerializeField] private GameObject winGroup;
+    [SerializeField] private Button restartButton;
 
     private Transform[] buttons, positions;
-    private Vector2[] buttonsCoords, positionsCoords;
+    private Vector2[] buttonsCoordsShuffle;
 
     private Vector2 emptyPositionCoords;
-    public bool checkEmptyDone;
 
     private Button[] buttonsOnGame;
 
@@ -21,19 +23,40 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         StoreAllButtonsAndPositions();
-        SaveButtonsAndPositionsCoord();
+        ShuffleButtonsPositions();
+
+        FindEmptyPosCoords();
 
         buttonsOnGame = FindObjectsOfType<Button>();
         for(int i = 0; i < buttonsOnGame.Length; i++)
         {
             buttonsOnGame[i].enabled = false;
         }
+
+        FindNearestButtons();    
+        
+        winGroup.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Trova le coordinate della posizoine vuota
+    /// </summary>
+    private void FindEmptyPosCoords()
     {
-        FindNearestButtons();
+        for(int i = 0; i < positions.Length; i++)
+        {
+            for(int j = 0; j < buttons.Length; j++)
+            {
+                if(buttons[j].position == positions[i].position)
+                {
+                    //do nothing
+                }
+                else
+                {
+                    emptyPositionCoords = positions[i].position;
+                }
+            }
+        }
     }
 
     /// <summary>
@@ -55,48 +78,75 @@ public class GameManager : MonoBehaviour
     }
 
     /// <summary>
-    /// Salvo le coordinate delle posizioni e dei pulsanti
+    /// Trova il/i pulsanti più vicino/i alla posizione vuota e lo/li abilita
     /// </summary>
-    private void SaveButtonsAndPositionsCoord()
-    {
-        buttonsCoords = new Vector2[buttons.Length];
-        for(int i = 0; i < buttons.Length; i++)
+    private void FindNearestButtons()
+    {          
+        for (int i = 0; i < buttons.Length; i++)
         {
-            buttonsCoords[i] = buttons[i].transform.position;
+            x = Mathf.Abs(buttons[i].transform.position.x - emptyPositionCoords.x);
+            y = Mathf.Abs(buttons[i].transform.position.y - emptyPositionCoords.y);
+
+            if ((x == 160 && y == 0) || (x == 0 && y == 160))
+            {
+                print("nome pulsante: " + buttons[i].name);
+                for(int j = 0; j < buttonsOnGame.Length; j++)
+                {
+                    if (buttonsOnGame[j].name == buttons[i].name)
+                    {
+                        buttonsOnGame[j].enabled = true;
+                    }
+                }
+            }
+        }               
+    }
+
+    /// <summary>
+    /// Mescola le posizioni dei pulsanti sul campo
+    /// </summary>
+    private void ShuffleButtonsPositions()
+    {
+        buttonsCoordsShuffle = new Vector2[buttons.Length];
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttonsCoordsShuffle[i] = buttons[i].transform.position;
         }
 
-        positionsCoords = new Vector2[positions.Length];
-        for (int i = 0; i < positions.Length; i++)
+        for (int i = 0; i < buttonsCoordsShuffle.Length - 1; i++)
         {
-            positionsCoords[i] = positions[i].transform.position;
+            int rnd = Random.Range(i, buttonsCoordsShuffle.Length);
+            Vector2 tempGO = buttonsCoordsShuffle[rnd];
+            buttonsCoordsShuffle[rnd] = buttonsCoordsShuffle[i];
+            buttonsCoordsShuffle[i] = tempGO;            
+        }
+
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].position = buttonsCoordsShuffle[i];
         }
     }
 
     /// <summary>
-    /// Trova il/i pulsanti più vicino/i alla posizione vuota e lo/li abilita
+    /// Verifica se tutte le carte sono disposte nell'ordine giusto
     /// </summary>
-    private void FindNearestButtons()
+    private void CheckWinConditions()
     {
-        if (checkEmptyDone)
-        {            
-            for (int i = 0; i < buttons.Length; i++)
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            if(buttons[i].position == positions[i].position)
             {
-                x = Mathf.Abs(buttons[i].transform.position.x - emptyPositionCoords.x);
-                y = Mathf.Abs(buttons[i].transform.position.y - emptyPositionCoords.y);
 
-                if ((x == 160 && y == 0) || (x == 0 && y == 160))
-                {
-                    print("nome pulsante: " + buttons[i].name);
-                    for(int j = 0; j < buttonsOnGame.Length; j++)
-                    {
-                        if (buttonsOnGame[j].name == buttons[i].name)
-                        {
-                            buttonsOnGame[j].enabled = true;
-                        }
-                    }
-                }
             }
-        }        
+            else
+            {
+                print("non hai ancora vinto");
+                return;
+            }
+        }
+
+        print("hai vinto");
+        restartButton.enabled = true;
+        winGroup.SetActive(true);
     }
 
     /// <summary>
@@ -114,15 +164,16 @@ public class GameManager : MonoBehaviour
             buttonsOnGame[i].enabled = false;
         }
 
-        FindNearestButtons();
+        CheckWinConditions();
+
+        FindNearestButtons();        
     }
 
     /// <summary>
-    /// Trova le coordinate della posizione vuota
+    /// Rincomincia la partita
     /// </summary>
-    /// <param name="posCoords">coordinate della posizione</param>
-    public void FillEmptyPositionCoords(Vector2 posCoords)
+    public void RestartMatch()
     {
-        emptyPositionCoords = posCoords;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
